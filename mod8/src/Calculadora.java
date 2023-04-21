99c72c0026dbac4803294ddc45e8380b1ced3811
Lincoln <its-lincoln@hotmail.com>
2023-04-21 20:12:17 -0300

    nova atualização de módulos

--- mod8/src/Calculadora.java
+++ mod8/src/Calculadora.java
@@ -2,7 +2,7 @@ public class Calculadora {
 
 
     public void mediaAritmetica (Float a, Float b, Float c, Float d){
-        
+
         System.out.println("A média aritmética desses valores é: "+(a+b+c+d)/4);
     }
 
